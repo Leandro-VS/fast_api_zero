08b3f4042de73db4726f5a207637578a26b305f9
Leandro-VS <leandro.vieira.silva.new@gmail.com>
2024-06-19 19:59:45 -0300

    exercicio

--- fast_api_zero/app.py
+++ fast_api_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_api_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/exercise', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_exercise():
+    return "<html lang='pt-BR'>\
+            <head>\
+                <title>Exibição de Conteúdo</title>\
+            </head>\
+            <body>\
+                <div>\
+                    <p>author: Leandro</p>\
+                </div>\
+            </body>\
+            </html>"
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,27 @@ from fast_api_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) # Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') # Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_exercise_deve_retornar_ok_e_leandro():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/exercise')  # Act
+    print(response)
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.text == "<html lang='pt-BR'>\
+            <head>\
+                <title>Exibição de Conteúdo</title>\
+            </head>\
+            <body>\
+                <div>\
+                    <p>author: Leandro</p>\
+                </div>\
+            </body>\
+            </html>"  # Assert
